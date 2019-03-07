96e353c224bba968a9dffd6a2b429cd72bf89084
graygabrielle <gray.atkinson.adams@gmail.com>
2019-03-06 20:49:14 -0800

    changed answers to numbers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,67 +4,67 @@ let questions = [
     {
         question: "What is the capital of Armenia?",
         possibles: ["Yerevan", "Sisian", "Tbilisi", "Baku"],
-        answer: "Yerevan"
+        answer: 0
 
     },
     {
         question: "What is the capital of Kazakhstan?",
         possibles: ["Baku", "Astana", "Karagandy", "Omsk"],
-        answer: "Astana"
+        answer: 1
 
     },
     {
         question: "What is the capital of Finland?",
         possibles: ["Stockholm", "Oslo", "Helsinki", "Copenhagen"],
-        answer: "Helsinki"
+        answer: 2
 
     },
     {
         question: "What is the capital of Turkey?",
         possibles: ["Ankara", "Istanbul", "Antalya", "Izmir"],
-        answer: "Ankara"
+        answer: 0
 
     },
     {
         question: "What is the capital of Slovakia?",
         possibles: ["Ljubljana", "Zagreb", "Bratislava", "Sarajevo"],
-        answer: "Bratislava"
+        answer: 2
 
     },
     {
         question: "What is the capital of Angola?",
         possibles: ["Libreville", "Kinshasa", "Bangui", "Luanda"],
-        answer: "Luanda"
+        answer: 3
 
     },
     {
         question: "What is the capital of Ecuador?",
         possibles: ["Quito", "Bogota", "Caracas", "Santiago"],
-        answer: "Quito"
+        answer: 0
 
     },
     {
         question: "What is the capital of Papua New Guinea?",
         possibles: ["Tabubil", "Lea Lea", "Victoria", "Port Moresby"],
-        answer: "Port Moresby"
+        answer: 3
 
     },
     {
         question: "What is the capital of Nepal?",
         possibles: ["Bhutan", "Dhaka", "Kathmandu", "Kanpur"],
-        answer: "Kathmandu"
+        answer: 2
 
     },
     {
         question: "What is the capital of Vietnam?",
         possibles: ["Saigon", "Ho Chi Minh City", "Nha Trang", "Hanoi"],
-        answer: "Hanoi"
+        answer: 3
 
     },
     {
         question: "What is the capital of Nigeria?",
         possibles: ["Niamey", "Abuja", "Agadez", "Maradi"],
-        answer: "Abuja"
+        answer: 1
 
     },
  
@@ -96,67 +96,70 @@ function nextSlide() {
     $(".box").empty();
 
     let q = $("<h1 class='quest'>").append(questions[i].question);
-    let a1 = $("<button class='options'>").append(questions[i].possibles[0]);
-    let a2 = $("<button class='options'>").append(questions[i].possibles[1]);
-    let a3 = $("<button class='options'>").append(questions[i].possibles[2]);
-    let a4 = $("<button class='options'>").append(questions[i].possibles[3]);
+    let a1 = $("<button class='options' id=0>").append(questions[i].possibles[0]);
+    let a2 = $("<button class='options' id=1>").append(questions[i].possibles[1]);
+    let a3 = $("<button class='options' id=3>").append(questions[i].possibles[2]);
+    let a4 = $("<button class='options' id=4>").append(questions[i].possibles[3]);
 
     $(".box").append(q);
     $(".box").append(a1);
     $(".box").append(a2);
     $(".box").append(a3);
     $(".box").append(a4);
-    timer1 = setTimeout(answerPage, 500);
+    timer1 = setTimeout(timeoutPage, 1000);
     
+    $(".options").click(function(event) {
+        clearTimeout(timer1);
+        console.log(event.target.id);
+        
+        // let userChoice = $("this").val();
+        // console.log(userChoice);
+        // showAnswer();
+    });
 }
 
-function answerPage() {
+
+function timeoutPage() {
     $(".box").empty();
-    if (i<=10){
+
+    //show timeout answer screen
+    if (i<10){
         i++;
-        timer2 = setTimeout(nextSlide, 500);
+        timer2 = setTimeout(nextSlide, 1000);
         console.log(i);
+   
     }
-    if (i===11) {
+    if (i===10) {
         clearTimeout(timer2);
         console.log("Game over!");
+        finalScreen();
         
     }
     console.log("Answer Page!");
 
 }
 
-    // $(".options").on("click", function(){
-    //     let userChoice = $("this");
-    //     console.log(userChoice);
-    // });
+function showAnswer() {
+    $(".box").empty();
+    console.log("clicked button");
+    //show answer screen
+    if (i<10){
+        i++;
+        timer2 = setTimeout(nextSlide, 1000);
+        console.log(i);
+    }
+    if (i===10) {
+        clearTimeout(timer2);
+        console.log("Game over!");
+        finalScreen();
+        
+    }
+    console.log("Answer Page!");
+}
 
 
-// function timer() {
+function finalScreen() {
 
-//         let q = $("<h1 class='quest'>").append(questions[i].question);
-//         let a1 = $("<button class='options'>").append(questions[i].possibles[0]);
-//         let a2 = $("<button class='options'>").append(questions[i].possibles[1]);
-//         let a3 = $("<button class='options'>").append(questions[i].possibles[2]);
-//         let a4 = $("<button class='options'>").append(questions[i].possibles[3]);
-    
-//         $(".box").empty();
-//         $(".box").append(q);
-//         $(".box").append(a1);
-//         $(".box").append(a2);
-//         $(".box").append(a3);
-//         $(".box").append(a4);
-//         i++;
-//         if (i===11) {
-//             clearInterval(timer);
-//         }
-//         setInterval(timer, 1000);
-//     }
+}
 
-
-
-    // $(".options").on("click", function(){
-    //     let userChoice = $("this");
-    //     console.log(userChoice);
-    // });
 
